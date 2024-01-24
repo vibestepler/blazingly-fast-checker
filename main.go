6b88dfcf897dba41d092f3f2a9a9c87f2b771c02
vibestepler <144337960+vibestepler@users.noreply.github.com>
2024-01-24 17:17:53 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
+
+func GetInput(misaka chan string) (input string) {
+
+	fmt.Printf("Add your cc list: ")
+	fmt.Scan(&input)
+	resp, err := http.Get(input)
+
+	if err != nil {
+		log.Fatal(err)
+		misaka <- fmt.Sprintf("Error: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var buf bytes.Buffer
+
+	_, err = io.Copy(&buf, resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+		misaka <- fmt.Sprintf("Error reading response body: %v", err)
+		return
+	}
+
+	responseString := buf.String()
+
+	scanner := bufio.NewScanner(&buf)
+
+	lineCount := 0
+
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	misaka <- fmt.Sprintf("%v\n\nTotal CC: %v", responseString, lineCount)
+
+	close(misaka)
+
+	return
+
+}
+
+func main() {
+
+	misaka := make(chan string)
+
+	go GetInput(misaka)
+
+	message := <-misaka
+
+	fmt.Println(message)
+
+}
